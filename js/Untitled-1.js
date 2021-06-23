6811c47c73c8860931b3e8661d17fb36b3b01d77
Philadelphia Tivoli <85254393+philadelphiativoli@users.noreply.github.com>
2021-06-23 12:17:01 +0200

    update

---
+++ js/Untitled-1.js
@@ -0,0 +1,2 @@
+const sayHello = "Hello there!"
+console.log(sayHello)
---
+++ js/firstlesson.js
@@ -0,0 +1,10 @@
+const sayHello = "Hello there!"
+console.log(sayHello)
+
+2 variables
+
+const sayWelcome ="Welcome to "
+console.log(sayWelcome)
+const sayTlesson ="Today's Lesson"
+console.log(sayTlesson)
+sayWelcome + sayTlesson
